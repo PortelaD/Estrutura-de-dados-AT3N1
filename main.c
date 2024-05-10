ab54de19753291dd80eba882fb044338fe56f674
Matheus Da Cruz Santos <99676579+matheus-bdr@users.noreply.github.com>
2024-05-09 21:17:38 -0300

    Commit funções
    liberar_quarto()
    mostrar_quartos_vazios()
    exibir_menu()

--- main.c
+++ main.c
@@ -160,3 +160,73 @@ void editar_hospede() {
 
     printf("Hospede nao encontrado.\n");
 }
+void liberar_quarto() {
+    int quarto;
+    int encontrado = 0;
+
+    printf("Digite o numero do quarto a ser liberado: ");
+    scanf("%d", &quarto);
+
+    for (int i = 0; i < total_hospedes; i++) {
+        if (hospedes[i].quarto == quarto) {
+            encontrado = 1;
+            for (int j = i; j < total_hospedes - 1; j++) {
+                strcpy(hospedes[j].nome, hospedes[j + 1].nome);
+                hospedes[j].quarto = hospedes[j + 1].quarto;
+            }
+            total_hospedes--;
+            break;
+        }
+    }
+
+    if (encontrado) {
+        printf("Quarto %d liberado com sucesso.\n", quarto);
+        salvar_hospedes_no_arquivo();
+    } else {
+        printf("Quarto nao encontrado.\n");
+    }
+}
+
+void mostrar_quartos_vazios() {
+    int quartos[MAX_HOSPEDES];
+    int num_quartos = 0;
+
+    for (int i = 0; i < MAX_HOSPEDES; i++) {
+        quartos[i] = i + 1;
+    }
+
+    for (int i = 0; i < total_hospedes; i++) {
+        for (int j = 0; j < MAX_HOSPEDES; j++) {
+            if (quartos[j] == hospedes[i].quarto) {
+                quartos[j] = 0;
+                break;
+            }
+        }
+    }
+
+    printf("Quartos vazios: ");
+    for (int i = 0; i < MAX_HOSPEDES; i++) {
+        if (quartos[i] != 0) {
+            printf("%d ", quartos[i]);
+            num_quartos++;
+        }
+    }
+
+    if (num_quartos == 0) {
+        printf("Todos os quartos estao ocupados.\n");
+    } else {
+        printf("\n");
+    }
+}
+
+void exibir_menu() {
+    printf("\n==== Menu ====\n");
+    printf("1. Inserir hospedes em um quarto vazio\n");
+    printf("2. Listar hospedes por ordem alfabetica\n");
+    printf("3. Buscar hospede\n");
+    printf("4. Editar hospede\n");
+    printf("5. Liberar um quarto\n");
+    printf("6. Mostrar os numeros dos quartos vazios\n");
+    printf("7. Sair\n");
+    printf("Escolha uma opcao: ");
+}
